f1b11d07c88b1d4b7b5b74e68107b0ab9fbb900e
21A91A6157 <97424691+21A91A6157@users.noreply.github.com>
2022-09-05 22:53:27 +0530

    import java.util.*; class Input {     public static void main(String args[])     {         Scanner input=new Scanner(System.in);         String str=input.nextLine();         System.out.println(str);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Input
+{
+    public static void main(String args[])
+    {
+        Scanner input=new Scanner(System.in);
+        String str=input.nextLine();
+        System.out.println(str);
+    }
+}
