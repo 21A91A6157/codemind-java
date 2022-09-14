324269c7eb27421b5c473c0f1588d4653485fab8
21A91A6157 <97424691+21A91A6157@users.noreply.github.com>
2022-09-14 23:37:14 +0530

    import java.util.*; class Find {     public static void main(String[] args)     {         Scanner input=new Scanner(System.in);         int n=input.nextInt();         int sum=0;         for(int i=0;i<=n;i++)         {             sum=sum+i;         }         System.out.println(sum);     } }

---
+++ Find_sum_of_natural_numbers_.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Find
+{
+    public static void main(String[] args)
+    {
+        Scanner input=new Scanner(System.in);
+        int n=input.nextInt();
+        int sum=0;
+        for(int i=0;i<=n;i++)
+        {
+            sum=sum+i;
+        }
+        System.out.println(sum);
+    }
+}
