89fc93d70a20427e6ff7ace307faff8d52bf893a
21A91A6157 <97424691+21A91A6157@users.noreply.github.com>
2022-09-14 19:10:32 +0530

    import java.util.*; class Pava {     public static void main(String[] args)     {         Scanner input=new Scanner(System.in);         int a=input.nextInt();         if(a>0)         {             System.out.println("Positive Number");         }         else         {             System.out.println("Negative Number");         }     } }

---
+++ Program_to_check_whether_the_given_integer_is_positive_or_negative.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Pava
+{
+    public static void main(String[] args)
+    {
+        Scanner input=new Scanner(System.in);
+        int a=input.nextInt();
+        if(a>0)
+        {
+            System.out.println("Positive Number");
+        }
+        else
+        {
+            System.out.println("Negative Number");
+        }
+    }
+}
