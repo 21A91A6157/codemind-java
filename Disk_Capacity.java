c03064336eebb48c5b680ea6c9efa51c1bad1dba
21A91A6157 <97424691+21A91A6157@users.noreply.github.com>
2022-11-04 07:05:03 +0530

    import java.util.*; class Code {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int m=sc.nextInt();         int b=sc.nextInt();         int s=n*m*b*2*512;         System.out.println(s);     } }

---
+++ Disk_Capacity.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Code
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        int b=sc.nextInt();
+        int s=n*m*b*2*512;
+        System.out.println(s);
+    }
+}
